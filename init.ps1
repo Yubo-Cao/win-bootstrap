e7fc3d49d3d05bce4696c05d8e1199aad2b3e9bc
Yubo-Cao <cao2006721@gmail.com>
2023-01-17 08:09:12 -0800

    feat: enable developer mode

--- init.ps1
+++ init.ps1
@@ -19,3 +19,26 @@ git config --global user.email 'cao2006721@gmail.com'
 scoop bucket add extras
 scoop bucket add versions
 scoop bucket add nerd-fonts
+
+# enable developer mode
+Invoke-Elevated {
+    Write-Output "Enabling developer mode"
+    if (([Version](Get-CimInstance Win32_OperatingSystem).version).Major -lt 10) {
+        Write-Host -ForegroundColor Red "The DeveloperMode is only supported on Windows 10"
+        exit 1
+    }
+
+    $RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+    if (! (Test-Path -Path $RegistryKeyPath)) {
+        New-Item -Path $RegistryKeyPath -ItemType Directory -Force
+    }
+
+    if (! (Get-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense)) {
+        # Add registry value to enable Developer Mode
+        New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
+    }
+    $feature = Get-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online
+    if ($feature -and ($feature.State -eq "Disabled")) {
+        Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -All -LimitAccess -NoRestart
+    }
+}
